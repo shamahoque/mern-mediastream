a214289edbd17defa4fc30c86f9884fc55067234
Shama Hoque <shama.hoque@gmail.com>
2018-04-30 14:53:13 +0600

    Get video API: stream from GridFS

--- server/controllers/media.controller.js
+++ server/controllers/media.controller.js
@@ -39,6 +39,68 @@ const create = (req, res, next) => {
     })
 }
 
+const mediaByID = (req, res, next, id) => {
+  Media.findById(id).populate('postedBy', '_id name').exec((err, media) => {
+    if (err || !media)
+      return res.status('400').json({
+        error: "Media not found"
+      })
+    req.media = media
+    next()
+  })
+}
+
+const video = (req, res) => {
+  gridfs.findOne({
+        _id: req.media._id
+    }, (err, file) => {
+        if (err) {
+            return res.status(400).send({
+                error: errorHandler.getErrorMessage(err)
+            })
+        }
+        if (!file) {
+            return res.status(404).send({
+                error: 'No video found'
+            })
+        }
+
+        if (req.headers['range']) {
+            var parts = req.headers['range'].replace(/bytes=/, "").split("-")
+            var partialstart = parts[0]
+            var partialend = parts[1]
+
+            var start = parseInt(partialstart, 10)
+            var end = partialend ? parseInt(partialend, 10) : file.length - 1
+            var chunksize = (end - start) + 1
+
+            res.writeHead(206, {
+                'Accept-Ranges': 'bytes',
+                'Content-Length': chunksize,
+                'Content-Range': 'bytes ' + start + '-' + end + '/' + file.length,
+                'Content-Type': file.contentType
+            })
+
+            gridfs.createReadStream({
+                _id: file._id,
+                range: {
+                    startPos: start,
+                    endPos: end
+                }
+            }).pipe(res)
+        } else {
+            res.header('Content-Length', file.length)
+            res.header('Content-Type', file.contentType)
+
+            gridfs.createReadStream({
+                _id: file._id
+            }).pipe(res)
+        }
+    })
+}
+
 export default {
-  create
+  create,
+  mediaByID,
+  video
 }
--- server/routes/media.routes.js
+++ server/routes/media.routes.js
@@ -8,6 +8,10 @@ const router = express.Router()
 router.route('/api/media/new/:userId')
     .post(authCtrl.requireSignin, mediaCtrl.create)
 
+router.route('/api/media/video/:mediaId')
+    .get(mediaCtrl.video)
+
 router.param('userId', userCtrl.userByID)
+router.param('mediaId', mediaCtrl.mediaByID)
 
 export default router
